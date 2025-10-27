c21c12fd60d23286b41864f08b60b8cd05a32f50
Haahm608 <hamseilyas@hotmail.com>
2025-10-27 17:06:11 +0100

    hh

--- obligatoriskoppgave2/klasse.php
+++ obligatoriskoppgave2/klasse.php
@@ -1,4 +1,4 @@
-<?php include "db.php"; ?>
+<?php include "dbtil.php";include ("index.php");  ?>
 <h2>Alle klasser</h2>
 <table border="1">
 <tr><th>Klassekode</th><th>Klassenavn</th><th>Studiumkode</th></tr>
